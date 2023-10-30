b70ac98615ae1bf3bb629169b66c51a335159267
VicCalderoon <146136453+VicCalderoon@users.noreply.github.com>
2023-10-29 23:49:03 -0700

    Agregar archivos de proyecto.

---
+++ PRACTICA10 SHARP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static int SumaIterativa(int n)
+    {
+        int suma = 0;
+
+        while (n > 9)
+        {
+            suma += n % 10;
+            n /= 10;
+        }
+
+        return suma + n;
+    }
+
+    static void Main()
+    {
+        int numero = 12345;
+        int resultado = SumaIterativa(numero);
+        Console.WriteLine($"La suma de los dígitos de {numero} es {resultado}");
+    }
+}
